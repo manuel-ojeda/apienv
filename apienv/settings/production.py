3e97af383123e08cd9bb32b1b146f16c38581a99
Manuel Ojeda <Manuel.Ojeda.892@gmail.com>
2016-09-26 20:15:10 -0500

    Fix production.py

--- apienv/settings/production.py
+++ apienv/settings/production.py
@@ -15,7 +15,7 @@ DATABASES = {
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR.child('static')]
 
-STATIC_ROOT = os.path.join(os.getcwd(), 'media/')
+MEDIA_ROOT = os.path.join(os.getcwd(), 'static/')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
